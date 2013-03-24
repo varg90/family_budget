dad30551cf5ced9ad5da5e5459e3c106f286852f
varg90 <kuhn.alexey@gmail.com>
2013-03-24 19:30:37 +0800

    create-purchase makeup

--- protected/views/purchase/create.php
+++ protected/views/purchase/create.php
@@ -1,4 +1,4 @@
-<div class="form">
+<div class="form" style="margin-left: 300px; margin-right: 300px">
     <?php
     /* @var $form TbActiveForm */
     $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', [
@@ -6,44 +6,46 @@
     ]);
     ?>
     <div class="control-group ">
-        <label class="control-label" for="Purchase_date">
-            <?php
-            echo $form->label($purchase, 'date');
-            ?>
-        </label>
+        <?php
+        echo $form->label($purchase, 'date', [
+            'class' => 'control-label',
+        ]);
+        ?>
         <div class="controls">
             <?php
             $this->widget('zii.widgets.jui.CJuiDatePicker', array(
                 'model' => $purchase,
                 'attribute' => 'date',
-                'options' => array(
+                'options' => [
                     'showAnim' => 'fold',
                     'dateFormat' => 'yy-mm-dd',
-                ),
-                'htmlOptions' => array(
+                ],
+                'htmlOptions' => [
                     'value' => date('Y-m-d'),
-                ),
+                ],
             ));
             ?>
         </div>
-    </div>
-    <?php
-    echo $form->textFieldRow($purchase, 'name');
-    echo $form->dropDownListRow($purchase, 'category', CHtml::listData(
-                    Category::model()->findAll(), 'id', 'name'
-    ));
-    echo $form->textFieldRow($purchase, 'cost');
-    ?>
-    <div class="form-actions">
         <?php
-        $this->widget('bootstrap.widgets.TbButton', [
-            'buttonType' => TbButton::BUTTON_SUBMIT,
-            'type' => TbButton::TYPE_PRIMARY,
-            'label' => "Сохранить",
-        ]);
+        echo $form->textFieldRow($purchase, 'name');
+        echo $form->dropDownListRow($purchase, 'category', CHtml::listData(
+                        Category::model()->findAll(), 'id', 'name'
+        ));
+        echo $form->textFieldRow($purchase, 'cost');
         ?>
     </div>
+</div>
+<div class="form-actions">
     <?php
-    $this->endWidget();
+    $this->widget('bootstrap.widgets.TbButton', [
+        'buttonType' => TbButton::BUTTON_SUBMIT,
+        'type' => TbButton::TYPE_PRIMARY,
+        'size' => TbButton::SIZE_LARGE,
+        'label' => "Сохранить",
+        'htmlOptions' => [
+            'style' => 'margin-left: 450px;',
+        ],
+    ]);
     ?>
 </div>
+<?php $this->endWidget(); ?>
