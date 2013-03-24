0faf9969abbb807eddbe21b84a53af691c3c3d6d
Yuri Ratanov <organium@gmail.com>
2013-03-24 18:11:16 +0800

    header -> partial

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -22,51 +22,8 @@
     <body>
 
         <div class="container" id="page">
-
-            <div id="header">
-                <div class="row">
-                    <div class="span-6" style="margin-left: 50px">
-                        <h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
-                    </div>
-                    <br>
-                        <div class="span-45">
-                            <?php
-                            $this->widget('bootstrap.widgets.TbButton', [
-                                'label' => 'Новая затрата',
-                                'url' => Yii::app()->createUrl('/purchase/create'),
-                                'size' => TbButton::SIZE_LARGE,
-                                'type' => TbButton::TYPE_INFO,
-                                'icon' => 'icon-shopping-cart icon-white',
-                                'htmlOptions' => [
-                                    'width' => 200,
-                                    ],
-                            ]);
-                            ?>
-                        </div>
-                        <div class="span-45">
-                            <?php
-                            $this->widget('bootstrap.widgets.TbButton', [
-                                'label' => 'Статистика',
-                                'url' => '#',
-                                'size' => TbButton::SIZE_LARGE,
-                                'type' => TbButton::TYPE_INFO,
-                                'icon' => 'icon-th-list icon-white',
-                            ]);
-                            ?>
-                        </div>
-                        <div class="span-4">
-                            <?php
-                            $this->widget('bootstrap.widgets.TbButton', [
-                                'label' => 'Зарплата',
-                                'url' => '#',
-                                'size' => TbButton::SIZE_LARGE,
-                                'type' => TbButton::TYPE_INFO,
-                                'icon' => 'icon-plus icon-white',
-                            ]);
-                            ?>
-                        </div>
-                </div>
-            </div><!-- header -->
+            <?php $this->renderPartial('//shared/header')?>
+            
             <?php echo $content; ?>
 
             <div class="clear"></div>
