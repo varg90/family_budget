5f9e0d1417cc75ad3b7d14eccb5cfac28c481cc3
varg90 <kuhn.alexey@gmail.com>
2013-03-27 01:14:11 +0800

    button labels

--- protected/messages/ru/view.php
+++ protected/messages/ru/view.php
@@ -6,4 +6,7 @@ return [
     'labels.salary' => 'Зарплата',
     'labels.save' => 'Сохранить',
     'labels.show' => 'Показать',
+    'labels.forDay' => 'За день',
+    'labels.forWeek' => 'За неделю',
+    'labels.forMonth' => 'За месяц',
 ];
--- protected/views/statistic/index.php
+++ protected/views/statistic/index.php
@@ -1,22 +1,47 @@
-<?php
-
-$this->widget('bootstrap.widgets.TbButton', [
-    'size' => TbButton::SIZE_LARGE,
-    'url' => Yii::app()->createUrl(''),
-    'label' => Yii::t('view',''),
-]);
-$this->widget('bootstrap.widgets.TbButton', [
-    'size' => TbButton::SIZE_LARGE,
-    'url' => Yii::app()->createUrl(''),
-    'label' => Yii::t('view',''),
-]);
-$this->widget('bootstrap.widgets.TbButton', [
-    'size' => TbButton::SIZE_LARGE,
-    'url' => Yii::app()->createUrl(''),
-    'label' => Yii::t('view',''),
-]);
-$this->widget('bootstrap.widgets.TbButton', [
-    'size' => TbButton::SIZE_LARGE,
-    'url' => Yii::app()->createUrl(''),
-    'label' => Yii::t('view',''),
-]);
+<div class="row">
+    <div class="span-4">
+        <?php
+        $this->widget('bootstrap.widgets.TbButton', [
+            'size' => TbButton::SIZE_LARGE,
+            'url' => Yii::app()->createUrl(''),
+            'label' => Yii::t('view', 'labels.forDay'),
+            'htmlOptions' => [
+                ],
+        ]);
+        ?>
+    </div>
+    <div class="span-4">
+        <?php
+        $this->widget('bootstrap.widgets.TbButton', [
+            'size' => TbButton::SIZE_LARGE,
+            'url' => Yii::app()->createUrl(''),
+            'label' => Yii::t('view', 'labels.forWeek'),
+            'htmlOptions' => [
+                ],
+        ]);
+        ?>
+    </div>
+    <div class="span-4">
+        <?php
+        $this->widget('bootstrap.widgets.TbButton', [
+            'size' => TbButton::SIZE_LARGE,
+            'url' => Yii::app()->createUrl(''),
+            'label' => Yii::t('view', 'labels.forMonth'),
+            'htmlOptions' => [
+                ],
+        ]);
+        ?>
+    </div>
+    <div class="span-4">
+        <?php
+        $this->widget('bootstrap.widgets.TbButton', [
+            'type' => TbButton::TYPE_SUCCESS,
+            'size' => TbButton::SIZE_LARGE,
+            'url' => Yii::app()->createUrl(''),
+            'label' => Yii::t('view', 'labels.salary'),
+            'htmlOptions' => [
+                ],
+        ]);
+        ?>
+    </div>
+</div>
