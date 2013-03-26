88be1ddf80f3c2a0c3257cb3e19a320fb4a4bb82
varg90 <kuhn.alexey@gmail.com>
2013-03-27 00:55:36 +0800

    statistic main page began

---
+++ protected/controllers/StatisticController.php
@@ -0,0 +1,9 @@
+<?php
+
+class StatisticController extends Controller
+{
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
+}
---
+++ protected/views/statistic/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$this->widget('bootstrap.widgets.TbButton', [
+    'size' => TbButton::SIZE_LARGE,
+    'url' => Yii::app()->createUrl(''),
+    'label' => Yii::t('view',''),
+]);
+$this->widget('bootstrap.widgets.TbButton', [
+    'size' => TbButton::SIZE_LARGE,
+    'url' => Yii::app()->createUrl(''),
+    'label' => Yii::t('view',''),
+]);
+$this->widget('bootstrap.widgets.TbButton', [
+    'size' => TbButton::SIZE_LARGE,
+    'url' => Yii::app()->createUrl(''),
+    'label' => Yii::t('view',''),
+]);
+$this->widget('bootstrap.widgets.TbButton', [
+    'size' => TbButton::SIZE_LARGE,
+    'url' => Yii::app()->createUrl(''),
+    'label' => Yii::t('view',''),
+]);
